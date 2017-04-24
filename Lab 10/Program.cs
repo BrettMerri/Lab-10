7bfffbe3b9f98341de1cd3c2c632c131bea524fe
BrettMerri <the.b.merri@gmail.com>
2017-04-24 14:25:20 -0400

    Alphabetized list of movies. Added two new methods to clean up main - GetCategoryList and GetMovieOutput

--- Lab 10/Program.cs
+++ Lab 10/Program.cs
@@ -18,19 +18,19 @@ namespace Lab_10
 
             List<Movie> MovieList = new List<Movie>(); //Creates a list of Movie objects named MovieList.
 
-            //Adds 12 movies to the MovieList each with a title and category
-            MovieList.Add(new Movie("The Matrix", "scifi"));
+            //Adds 12 movies to the MovieList in alphabetical order, each with a title and category
+            MovieList.Add(new Movie("Fight Club:", "drama"));
+            MovieList.Add(new Movie("Finding Nemo", "animated"));
             MovieList.Add(new Movie("Intersteller", "scifi"));
             MovieList.Add(new Movie("Star Wars", "scifi"));
-            MovieList.Add(new Movie("Toy Story", "animated"));
-            MovieList.Add(new Movie("Finding Nemo", "animated"));
-            MovieList.Add(new Movie("The Lion King", "animated"));
-            MovieList.Add(new Movie("Titanic", "drama"));
-            MovieList.Add(new Movie("The Godfather", "drama"));
-            MovieList.Add(new Movie("Fight Club:", "drama"));
-            MovieList.Add(new Movie("The Shining", "horror"));
-            MovieList.Add(new Movie("The Exorcist", "horror"));
             MovieList.Add(new Movie("The Conjuring", "horror"));
+            MovieList.Add(new Movie("The Exorcist", "horror"));
+            MovieList.Add(new Movie("The Godfather", "drama"));
+            MovieList.Add(new Movie("The Lion King", "animated"));
+            MovieList.Add(new Movie("The Matrix", "scifi"));
+            MovieList.Add(new Movie("The Shining", "horror"));
+            MovieList.Add(new Movie("Titanic", "drama"));
+            MovieList.Add(new Movie("Toy Story", "animated"));
 
             string[] Options = { "Animated", "Drama", "Horror", "SciFi" }; //Declare movie categories in an Options array
             bool loop = true; //Set loop to true
@@ -42,22 +42,31 @@ namespace Lab_10
                 Console.WriteLine("There are 100 movies in this list.");
                 Console.WriteLine("What category are you interested in?");
 
-                for (int i = 0; i < Options.Length; i++)
-                {
-                    Console.WriteLine($"{i + 1}.) {Options[i]}");
-                }
+                GetCategoryList(Options);
 
                 int CategoryInput = ValidateConsoleInput.GetIntegerInRange(1, 4); //Gets input from user and validates that the input is one of the categories in the options array
 
-                foreach (Movie item in MovieList) //For each movie in the movie list
-                {
-                    if (item.MovieCategory == Options[CategoryInput - 1].ToLower()) //If the movie category equals the category input
-                    {
-                        Console.WriteLine(item.MovieTitle); //Write the movie title
-                    }
-                }
+                GetMovieOutput(MovieList, Options, CategoryInput);
+                
                 if (!ContinueApp()) //Prompts user if he wants to continue. Set loop to false if use does not want to continue.
                     loop = false;
+            }
+        }
+        public static void GetCategoryList(string[] Options)
+        {
+            for (int i = 0; i < Options.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}.) {Options[i]}");
+            }
+        }
+        public static void GetMovieOutput(List<Movie> MovieList, string[] Options, int CategoryInput)
+        {
+            foreach (Movie item in MovieList) //For each movie in the movie list
+            {
+                if (item.MovieCategory == Options[CategoryInput - 1].ToLower()) //If the movie category equals the category input
+                {
+                    Console.WriteLine(item.MovieTitle); //Write the movie title
+                }
             }
         }
         public static bool ContinueApp()
